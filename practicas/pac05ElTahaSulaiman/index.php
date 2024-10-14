1ae27698adb271d8c50b0d2aeead1f9ce3b241f7
Sulaiman256 <123740914+Sulaiman256@users.noreply.github.com>
2024-10-14 13:36:25 +0000

    Practica 5 me  quede en el ejercicio 7

--- practicas/pac05ElTahaSulaiman/index.php
+++ practicas/pac05ElTahaSulaiman/index.php
@@ -16,18 +16,53 @@ function calcularTotal($precio, $cantidad, $impuestos){
     $precio = 15;
     $cantidad = 2;
     $total = $precio * $cantidad * ($impuestos);
-    return $total;
+    echo '<h2>El total es: '.$total.'</h2>';
 
 }
 calcularTotal($precio, $cantidad, $impuestos);
 
 function generarResumen($texto, $limite){
-    // hay que hacer un resumen acortado con ternarios
     $resumen = (strlen($texto) > $limite)? substr($texto, 0, $limite). '...' : $texto;
-    return $resumen;
+    echo '<h3>Resumen: '.$resumen.'</h3>';
+}
+generarResumen('Sulaiman', 5);
+
+function convertirTemperatura($temperatura, $escala){
+    if($escala == 'Celsius'){
+        $convertido = ($temperatura * 9/5) + 32;
+        echo '<h2>La temperatura es: '.$convertido.' Fahrenheit</h2>';
+    } elseif($escala == 'Fahrenheit'){
+        $convertido = ($temperatura -32 ) * 5/9;
+        echo '<h2>La temperatura es: '.$convertido.' Celsius</h2>';
+    } else {
+        echo '<h2>Escala no válida</h2>';
+    }
+}
+convertirTemperatura(100, 'Fahrenheit');
+convertirTemperatura(100, 'Celsius');
+
+
+function calcularEdad($anioNacimiento){
+    $hoy = date('Y');
+    $edad = $hoy - $anioNacimiento;
+    echo '<h2>Tu edad es: '.$edad.'</h2>';
+}
+calcularEdad(2002);
+function esPar($numero){
+    if($numero % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
 }
 
+$resultado = esPar(3);
+echo '<h2>El número 3 es par: '.($resultado? 'Sí' : 'No').'</h2>';
 
-
-
+function generarEnlaceDescarga($archivo){
+    $url = './images.jpeg'.basename($archivo);
+    echo '<h2>Descargar archivo: <a href="'.$url.'">'.$archivo.'</a></h2>';
+}
+generarEnlaceDescarga('perro')
 ?>
+
