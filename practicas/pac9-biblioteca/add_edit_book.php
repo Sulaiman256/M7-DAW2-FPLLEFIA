0feed9af9b8ce12d5c83788d2f8426feabbfef20
Sulaiman256 <123740914+Sulaiman256@users.noreply.github.com>
2024-11-25 18:52:09 +0000

    script funciones hecho

--- practicas/pac9-biblioteca/add_edit_book.php
+++ practicas/pac9-biblioteca/add_edit_book.php
@@ -1,58 +1,9 @@
 <?php
 
-function agregarLibro($titulo, $autor, $imagen, $descripcion)
-{
-    include './array.php';
 
-    if (!isset($libros) || !is_array($libros)) {
-        $libros = [];
-    }
-
-    $nuevoLibro = [
-        'id' => count($libros) + 1,
-        'titulo' => $titulo,
-        'autor' => $autor,
-        'imagen' => $imagen,
-        'descripcion' => $descripcion,
-    ];
-
-    $libros[] = $nuevoLibro;
-
-    guardarLibros($libros);
-
-    return true;
-}
-
-function editarLibro($id, $titulo, $autor, $imagen, $descripcion)
-{
-    include './array.php';
-
-    $encontrado = false;
-    foreach ($libros as &$libro) {
-        if ($libro['id'] == $id) {
-            $libro['titulo'] = $titulo;
-            $libro['autor'] = $autor;
-            $libro['imagen'] = $imagen;
-            $libro['descripcion'] = $descripcion;
-            $encontrado = true;
-            break;
-        }
-    }
-
-    if ($encontrado) {
-        guardarLibros($libros);
-    }
-
-    return $encontrado;
-}
-
-function guardarLibros($libros)
-{
-    $contenido = '<?php' . PHP_EOL . '$libros = ' . var_export($libros, true) . ';' . PHP_EOL;
-    file_put_contents('./array.php', $contenido);
-}
 
 include_once './array.php';
+include_once './functions.php';
 
 session_start();
 
--- practicas/pac9-biblioteca/array.php
+++ practicas/pac9-biblioteca/array.php
@@ -1,63 +1,63 @@
 <?php
-$libros = array(
-  0 =>
-  array(
+$libros = array (
+  0 => 
+  array (
     'id' => 1,
     'titulo' => 'Cien años de soledad',
     'autor' => 'Gabriel García Márquez',
     'descripcion' => 'Una novela que narra la historia de la familia Buendía en el mítico pueblo de Macondo, una obra maestra del realismo mágico.',
     'imagen' => 'https://m.media-amazon.com/images/I/81rEWmLXliL._AC_UF1000,1000_QL80_.jpg',
   ),
-  1 =>
-  array(
+  1 => 
+  array (
     'id' => 2,
     'titulo' => '198446',
     'autor' => 'George Orwell65',
     'descripcion' => 'Un thriller distópico que imagina un futuro totalitario donde el gobierno controla todos los aspectos de la vida, incluso el pensamiento.65',
     'imagen' => 'https://cdn.prod.website-files.com/6034d7d1f3e0f52c50b2adee/6254291caac6d1e42e8986df_62023ceb41cd1c2807b2841a_9788418933011.jpeg',
   ),
-  2 =>
-  array(
+  2 => 
+  array (
     'id' => 3,
     'titulo' => 'Orgullo y prejuicio656',
     'autor' => 'Jane Austen5646',
     'descripcion' => 'Una novela que examina las tensiones sociales y amorosas entre Elizabeth Bennet y el orgulloso Darcy en la Inglaterra del siglo XIX.hghg',
     'imagen' => 'https://cdn.prod.website-files.com/6034d7d1f3e0f52c50b2adee/6540f3a95dd2301ef9751dd6_H2tjHnUNhg_lBh5PnN9lJhXAfqX5EUHakOyByeEodU4.jpeg',
   ),
-  3 =>
-  array(
+  3 => 
+  array (
     'id' => 4,
     'titulo' => 'El señor de los anillos6456',
     'autor' => 'J.R.R. Tolkien6546',
     'descripcion' => 'Una épica aventura en la Tierra Media, donde un grupo de héroes lucha para destruir un anillo mágico que amenaza al mundo.646',
     'imagen' => 'https://www.libreriaalberti.com/media/img/portadas/_visd_0000JPG02Q3B.jpg',
   ),
-  4 =>
-  array(
+  4 => 
+  array (
     'id' => 5,
     'titulo' => 'La sombra del viento1erq',
     'autor' => 'Carlos Ruiz Zafónrqrq',
     'descripcion' => 'Una novela que combina misterio, amor y literatura en la Barcelona de la posguerra, donde un jovarren descubre un libro olvidado.',
     'imagen' => 'https://m.media-amazon.com/images/I/81wLkU+opPL._AC_UF894,1000_QL80_.jpg',
   ),
-  5 =>
-  array(
+  5 => 
+  array (
     'id' => 6,
     'titulo' => 'hola6464',
     'autor' => 'hola464',
     'imagen' => 'https://png.pngtree.com/background/20230524/original/pngtree-sad-pictures-for-desktop-hd-backgrounds-picture-image_2705986.jpg',
     'descripcion' => 'fjioafajiopf46',
   ),
-  6 =>
-  array(
+  6 => 
+  array (
     'id' => 7,
     'titulo' => 'hola',
     'autor' => 'hola',
     'imagen' => 'https://png.pngtree.com/background/20230524/original/pngtree-sad-pictures-for-desktop-hd-backgrounds-picture-image_2705986.jpg',
     'descripcion' => 'fjioafajiopf',
   ),
-  7 =>
-  array(
+  7 => 
+  array (
     'id' => 8,
     'titulo' => 'hola',
     'autor' => 'hola',
@@ -65,7 +65,3 @@ $libros = array(
     'descripcion' => 'fsgfs',
   ),
 );
-$usuarios = [
-  ["username" => "admin", "password" => "adminpass", "role" => "admin"],
-  ["username" => "reader", "password" => "readerpass", "role" => "lector"]
-];
