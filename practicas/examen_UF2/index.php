ff7a8653f8d6c5f9350bfa8f5ea16ebf91053b8f
Sulaiman256 <123740914+Sulaiman256@users.noreply.github.com>
2025-02-06 19:23:31 +0000

    Menu de listado ejercicios

---
+++ practicas/examen_UF2/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen UF2</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-50 text-gray-900 font-sans tracking-wide">
+
+    <div class="max-w-4xl mx-auto p-8">
+
+        <header class="text-center mb-12">
+            <h1 class="text-5xl font-extrabold text-blue-800 mb-4">Examen UF2</h1>
+            <p class="text-lg text-gray-600">Un vistazo a los proyectos realizados en el examen.</p>
+        </header>
+
+        <section class="bg-white rounded-lg shadow-lg p-6 mb-10">
+            <h2 class="text-3xl font-semibold text-blue-600 mb-6">EX2_MINIJOCS</h2>
+            <div class="space-y-4">
+                <a href="Ex2_Mini-jocs/joc1/index.php"
+                    class="block py-3 px-5 text-xl text-blue-800 rounded-lg hover:bg-blue-100 transition duration-300 ease-in-out">
+                    Ex1 - Mini Juego 1
+                </a>
+                <a href="Ex2_Mini-jocs/joc2/index.php"
+                    class="block py-3 px-5 text-xl text-blue-800 rounded-lg hover:bg-blue-100 transition duration-300 ease-in-out">
+                    Ex2 - Mini Juego 2
+                </a>
+                <a href="Ex2_Mini-jocs/joc3/index.php"
+                    class="block py-3 px-5 text-xl text-blue-800 rounded-lg hover:bg-blue-100 transition duration-300 ease-in-out">
+                    Ex3 - Mini Juego 3
+                </a>
+                <a href="Ex2_Mini-jocs/joc4/index.php"
+                    class="block py-3 px-5 text-xl text-blue-800 rounded-lg hover:bg-blue-100 transition duration-300 ease-in-out">
+                    Ex4 - Mini Juego 4
+                </a>
+            </div>
+        </section>
+
+        <section class="bg-white rounded-lg shadow-lg p-6">
+            <h2 class="text-3xl font-semibold text-blue-600 mb-6">EX3 Sistema de reserva de hoteles</h2>
+            <a href="Ex3_Sistema_reserva_de_hotels/index.php"
+                class="block py-3 px-5 text-xl text-blue-800 rounded-lg hover:bg-blue-100 transition duration-300 ease-in-out">
+                Ex1 - Sistema de Reserva
+            </a>
+        </section>
+
+    </div>
+
+</body>
+
+</html>
